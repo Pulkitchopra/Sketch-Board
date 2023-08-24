5a0c79046e08ce34af2bdc1392a3372093836de0
Pulkitchopra <pulkitchopra1990@gmail.com>
2023-08-24 20:10:05 +0530

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const socket = require('socket.io');
-
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
+
 app.use(express.static('public'));
 
 let port = process.env.PORT || 3000
@@ -10,19 +12,14 @@ let server = app.listen(port, () => {
     console.log('Port 3000');
 } )
 
-
 let io = socket(server);
-
 io.on('connection', (socket) => {
 
     console.log('Socket Connection');
 
 
 
-
-
     socket.on('beginPath', (data) => {
-
 
 
         io.sockets.emit('beginPath', data);
